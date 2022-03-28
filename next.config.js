10efab44194198fad1482ba3a7da1c6fbdc0ebf4
unknown <lizzbetgrant1009@gmail.com>
2022-03-28 19:04:10 +0800

    dush13

--- next.config.js
+++ next.config.js
@@ -4,12 +4,14 @@ const nextConfig = {
 }
 
 module.exports = {
+  basePath: '/wallisconsultancy',
   assetPrefix: '/wallisconsultancy/',
   images: {
     loader: 'akamai',
     path: '../public/Jiji.webp'
   },
   env: {
+    BACKEND_URL: '/wallisconsultancy',
     PUBLIC_URL: "https://webProgramming",
     assetPrefix: './'
   }
